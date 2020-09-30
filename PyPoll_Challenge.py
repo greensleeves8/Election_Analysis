a4d924cb50723ed09522d4f1a5b5343c312ac0e6
greensleeves8 <theonlycalegreen@gmail.com>
2020-09-30 01:15:55 -0500

    Module Challenge Final

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -104,15 +104,22 @@ with open(file_to_save, "w") as txt_file:
 
         print(county_results) 
          # 6e: Save the county votes to a text file.
-
+        txt_file.write(county_results)
          # 6f: Write a decision statement to determine the winning county and get its vote count.
-
-
+        if (county_votes > most_votes):
+            most_votes = county_votes
+            largest_turnout = county
+        
     # 7: Print the county with the largest turnout to the terminal.
+    turnout_summary = (
+       f"-------------------------\n"
+       f"Largest County Turnout: {largest_turnout}\n"
+       f"-------------------------\n")
 
+    print(turnout_summary)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(turnout_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
